ddfecc18caf4a505c4f895346e0865c3f51f99c9
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-19 13:53:47 +0100

    adds seed data

--- src/main/java/db/Seeds.java
+++ src/main/java/db/Seeds.java
@@ -1,4 +1,21 @@
 package db;
 
+import models.Advert;
+import models.Shop;
+
 public class Seeds {
+
+    public static void seedData() {
+
+        Shop shop = new Shop("Plumtree");
+        DBHelper.save(shop);
+
+        Advert advert1 = new Advert("Bike", "Very fast bike", 100.00, "/image/bike");
+        DBHelper.save(advert1);
+        Advert advert2 = new Advert("Dress", "Very nice dress", 50.00, "/image/dress");
+        DBHelper.save(advert2);
+
+    }
+
+
 }
