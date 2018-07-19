558ba13fe244fa2707b98cb9552a74d88ab53e26
Gary Muir <garymuir17@gmail.com>
2018-07-19 11:21:38 +0100

    adds getWallet

--- src/main/java/models/Shop.java
+++ src/main/java/models/Shop.java
@@ -1,4 +1,33 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shop {
+
+    private double wallet;
+    private List<Advert> adverts;
+
+//    public Shop() {}
+
+    public Shop() {
+        this.adverts = new ArrayList<Advert>();
+        this.wallet = 0.0;
+    }
+
+    public double getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(double wallet) {
+        this.wallet = wallet;
+    }
+
+    public List<Advert> getAdverts() {
+        return adverts;
+    }
+
+    public void setAdverts(List<Advert> adverts) {
+        this.adverts = adverts;
+    }
 }
--- src/test/java/models/ShopTest.java
+++ src/test/java/models/ShopTest.java
@@ -1,4 +1,24 @@
 package models;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class ShopTest {
+
+    Shop shop;
+
+    @Before
+    public void setup() {
+        this.shop = new Shop();
+    }
+
+    @Test
+    public void hasWallet() {
+        assertEquals(0.0, shop.getWallet(), 0.001);
+    }
+
+
+
 }
