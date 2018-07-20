255d4103525f921b945c5d3ef296c810b1a29e65
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-20 15:56:19 +0100

    adds all views for findByCategory

--- src/main/java/db/Seeds.java
+++ src/main/java/db/Seeds.java
@@ -22,6 +22,7 @@ public class Seeds {
         // adverts
         Advert advert1 = new Advert("Bike", "Very fast bike", 100.00, "/images/bike1.jpg");
         advert1.setShop(shop);
+        advert1.addCategory(CategoryType.TRANSPORT);
         DBHelper.save(advert1);
         Advert advert2 = new Advert("Dress", "Very nice dress", 50.00, "/images/dress1.jpg");
         advert2.setShop(shop);
@@ -31,30 +32,36 @@ public class Seeds {
         advert3.setShop(shop);
         advert3.addCategory(CategoryType.TECHNOLOGY);
         DBHelper.save(advert3);
+        Advert advert4 = new Advert("Hose", "Very long hose", 25.00, "/images/hose1.jpg");
+        advert4.setShop(shop);
+        advert4.addCategory(CategoryType.GARDEN);
+        DBHelper.save(advert4);
 
 
+
+        // --- WORKING TESTS BELOW --- //
 
         // update
-        advert1.setTitle("Banana");
-        DBHelper.update(advert1);
+//        advert1.setTitle("Banana");
+//        DBHelper.update(advert1);
 
         // delete
 //        DBHelper.delete(advert1);
 
         // findAll
-        List<Advert> foundAdverts = DBHelper.findAll(Advert.class);
+//        List<Advert> foundAdverts = DBHelper.findAll(Advert.class);
 
         // findById
-        Advert foundAdvert = DBHelper.findById(Advert.class, advert2.getId());
+//        Advert foundAdvert = DBHelper.findById(Advert.class, advert2.getId());
 
         // addAdvertToShop
 //        DBAdvert.addAdvertToShop(advert2, shop);
 
         // orderByPostedTime
-        List<Advert> orderedAdverts = DBAdvert.orderByPostedTime();
+//        List<Advert> orderedAdverts = DBAdvert.orderByPostedTime();
 
         // findByCategory
-        List<Advert> foundAdvertsInCategory = DBAdvert.findByCategory(CategoryType.TECHNOLOGY);
+//        List<Advert> foundAdvertsInCategory = DBAdvert.findByCategory(CategoryType.TECHNOLOGY);
 
 //        runs through the categorytype enum and creates a list filled with all of it's values
 //        List<CategoryType> categories = Arrays.asList(CategoryType.values());
