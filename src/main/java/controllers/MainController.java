fd8b98b138e665b074936bcd55f74a4dca6682f9
Gary Muir <garymuir17@gmail.com>
2018-07-20 09:01:55 +0100

    adds a layout.vtl

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -22,7 +22,7 @@ public class MainController {
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
 
-            model.put("template", "template/index.vtl");
+            model.put("template", "templates/index.vtl");
 
             return new ModelAndView(model,"templates/layout.vtl");
         }, velocityTemplateEngine);
